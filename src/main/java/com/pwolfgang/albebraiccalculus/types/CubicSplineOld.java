5447002580341396793548194b72326879651084
Paul Wolfgang <paul@pwolfgang.com>
2021-09-08 14:07:10 -0400

    Modified CubicSpline to work with non-uniformly spaced points.

--- src/main/java/com/pwolfgang/albebraiccalculus/types/CubicSplineOld.java
+++ src/main/java/com/pwolfgang/albebraiccalculus/types/CubicSplineOld.java
@@ -13,36 +13,33 @@ import java.util.Arrays;
  *
  * @author Paul Wolfgang <paul@pwolfgang.com>
  */
-public class CubicSpline {
-    
-    Point[] points;
+public class CubicSplineOld {
+     Point[] points;
     PolyNumber[] polies;
     PolyNumber[] pFinal;
     int n;
     
-    public CubicSpline(PolyNumber initialPoly, Point... points) {
+    public CubicSplineOld(PolyNumber initialPoly, Point... points) {
         this.points = points;
         n = points.length-1;
         polies = new PolyNumber[n];
         polies[0] = initialPoly;
+        Matrix P = Pascal.getP(4).trans();
+        System.out.printf("P_old: %s%n", P.toString());
         for (int i = 1; i < n; i++) {
-            Rational c = points[i].getX().sub(points[i-1].getX());
-            Matrix P = Pascal.getP(4,c).trans();
-            System.out.printf("P_new: %s%n",P.toString());
             Rational[] coefs = P.mul(polies[i-1].expandToDegree(4));
-            System.out.printf("coefs_new: %s%n", Arrays.asList(coefs).toString());
-            coefs[3] = Rational.ZERO;
-            var pp = new PolyNumber(coefs);
-            var dx = points[i+1].getX().sub(points[i].getX());
-            var s = pp.eval(dx);
-            var dXcubed = dx.mul(dx).mul(dx);
-            coefs[3] = (points[i+1].getY().sub(s)).div(dXcubed);
+            System.out.printf("coefs_old: %s%n", Arrays.asList(coefs).toString());
+            Rational sum = Rational.ZERO;
+            for (int j = 0; j < 3; j++) {
+                sum = sum.add(coefs[j]);
+            }
+            coefs[3] = points[i+1].getY().sub(sum);
             polies[i] = new PolyNumber(coefs);
         }
         pFinal = new PolyNumber[n];
         for (int i = 0; i < n; i++) {
-            pFinal[i] = polies[i].eval(new PolyNumber(points[i].getX().neg(), Rational.ONE));
+            pFinal[i] = polies[i].eval(new PolyNumber(-i, 1));
         }
     }
-    
+   
 }
--- src/test/java/com/pwolfgang/albebraiccalculus/types/CubicSplineTest.java
+++ src/test/java/com/pwolfgang/albebraiccalculus/types/CubicSplineTest.java
@@ -79,6 +79,7 @@ public class CubicSplineTest {
     
    @Test
     public void test3() {
+        System.out.println("test3 NEW");
         var p0 = new Point(0,1);
         var p1 = new Point(1,3);
         var p2 = new Point(2,2);
@@ -106,5 +107,61 @@ public class CubicSplineTest {
         System.out.printf("quartic') = %s%n", polyFromPointsPrime.toString());
         System.out.printf("quateic'(1) = %s%n", polyFromPointsPrimeOfOne.toString());
      }
+
+    @Test
+    public void test3_old() {
+        System.out.println("test3 OLD");
+        var p0 = new Point(0,1);
+        var p1 = new Point(1,3);
+        var p2 = new Point(2,2);
+        var p3 = new Point(3,2);
+        var p4 = new Point(4,1);
+        var p = new PolyNumber(Rational.ONE, new Rational(19,3), new Rational(-6), new Rational(5,3));
+        var s = new CubicSplineOld(p, p0, p1, p2, p3, p4);
+        for (int i = 0; i < s.polies.length; i++) {
+            System.out.printf("p[%d] = %s%n", i, s.polies[i].toString());
+        }
+        for (int i = 0; i < s.polies.length; i++) {
+            System.out.printf("p[%d] = %s%n", i, s.pFinal[i].toString());
+        }
+        for (int i = 0; i < s.polies.length; i++) {
+            PolyNumber xOft = new PolyNumber(i, 1);
+            PolyNumber yOft = s.pFinal[i].eval(xOft);
+            PolyNumber[] pp = new PolyNumber[]{xOft, yOft};
+            var c = new DCBcurve(pp);
+            System.out.printf("c[%d] = %s%n", i, c);
+        }
+        var polyFromPoints = PolyNumber.fromPoints(p0, p1, p2, p3, p4);
+        var polyFromPointsPrime = polyFromPoints.D();
+        var polyFromPointsPrimeOfOne = polyFromPointsPrime.eval(1);
+        System.out.printf("quartic = %s%n", polyFromPoints.toString());
+        System.out.printf("quartic') = %s%n", polyFromPointsPrime.toString());
+        System.out.printf("quateic'(1) = %s%n", polyFromPointsPrimeOfOne.toString());
+     }
     
+   @Test
+    public void test4() {
+        System.out.println("exercise 3");
+        var p0 = new Point(0,0);
+        var p1 = new Point(1,2);
+        var p2 = new Point(3,1);
+        var p3 = new Point(7,4);
+         var p = new PolyNumber(0, 0, 0, 2);
+        var s = new CubicSpline(p, p0, p1, p2, p3);
+        for (int i = 0; i < s.polies.length; i++) {
+            System.out.printf("p[%d] = %s%n", i, s.polies[i].toString());
+        }
+        for (int i = 0; i < s.polies.length; i++) {
+            System.out.printf("p[%d] = %s%n", i, s.pFinal[i].toString());
+        }
+        for (int i = 0; i < s.polies.length; i++) {
+            PolyNumber xOft = new PolyNumber(i, 1);
+            PolyNumber yOft = s.pFinal[i].eval(xOft);
+            PolyNumber[] pp = new PolyNumber[]{xOft, yOft};
+            var c = new DCBcurve(pp);
+            System.out.printf("c[%d] = %s%n", i, c);
+        }
+        
+        System.out.printf("cubic = %s%n", PolyNumber.fromPoints(p0, p1, p2, p3).toString());
+    }
 }
