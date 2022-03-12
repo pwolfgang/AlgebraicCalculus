39a2f9b302f1355b5d8dde48ada4fc07d988951b
Paul Wolfgang <paul@pwolfgang.com>
2022-03-12 08:36:28 -0500

    Renamed types.Matrix to types.NaiveMatrix

--- src/main/java/com/pwolfgang/albebraiccalculus/types/NaiveMatrix.java
+++ src/main/java/com/pwolfgang/albebraiccalculus/types/NaiveMatrix.java
@@ -8,11 +8,11 @@ package com.pwolfgang.albebraiccalculus.types;
 import java.util.Arrays;
 
 /**
- * A Matrix is a two dimensional array of Rationals
+ * A NaiveMatrix is a two dimensional array of Rationals
  * 
  * @author Paul Wolfgang <paul@pwolfgang.com>
  */
-public class Matrix {
+public class NaiveMatrix {
     
     private Rational[][]m;
     private final int nRows;
@@ -22,7 +22,7 @@ public class Matrix {
      * Create a new Matrix from an existing two dimensional array.
      * @param m The existing array.
      */
-    public Matrix(Rational[][] m) {
+    public NaiveMatrix(Rational[][] m) {
         nRows = m.length;
         nCols = m[0].length;
         this.m = new Rational[nRows][];
@@ -37,7 +37,7 @@ public class Matrix {
      * @param nRows The number of Rows;
      * @param nCols The number of Columns;
      */
-    public Matrix(int nRows, int nCols) {
+    public NaiveMatrix(int nRows, int nCols) {
         this.nRows = nRows;
         this.nCols = nCols;
         m = new Rational[nRows][nCols];
@@ -48,21 +48,21 @@ public class Matrix {
         }
     }
     
-    public Matrix swapRows(int i, int j) {
+    public NaiveMatrix swapRows(int i, int j) {
         Rational[] t = m[i];
         m[i] = m[j];
         m[j] = t;
         return this;
     }
     
-    public Matrix scaleRow(Rational s, int row) {
+    public NaiveMatrix scaleRow(Rational s, int row) {
         for (int j = 0; j < m[row].length; j++) {
             m[row][j] = m[row][j].mul(s);
         }
         return this;
     }
     
-    public Matrix scaleAndAdd(Rational s, int source, int dest) {
+    public NaiveMatrix scaleAndAddRow(Rational s, int source, int dest) {
         for (int j = 0; j < m[source].length; j++) {
             m[dest][j] = m[dest][j].add(m[source][j].mul(s));
         }
